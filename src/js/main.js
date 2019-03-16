980fe2faf686f06a53fd1b65eae845f53142aa18
Valery Alikin <weblcw@gmail.com>
2019-03-16 19:21:36 +0500

    remove jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,179 +1,187 @@
-$(function () {
-    $('body').on('click', '#off', function () {
-        $('.radio').addClass('off');
-    })
+let off = document.getElementById('off');
+let on = document.getElementById('on');
+let radio = document.querySelector('.radio');
 
-    $('body').on('click', '#on', function () {
-        $('.radio').removeClass('off');
-    });
+off.addEventListener('click', () => {
+    radio.classList.add('off');
+});
 
+on.addEventListener('click', () => {
+    radio.classList.remove('off');
+});
 
+let checkbox = document.querySelector('.checkbox');
+let checkboxOn = document.querySelector('.checkbox .on');
+let checkboxOff = document.querySelector('.checkbox .off');
+let checkboxLabel = document.querySelector('.checkbox label');
+let trigger = true;
 
+anime({ 
+    targets: checkboxOn, 
+    translateX: '0',
+    zIndex: {
+        value: [1, 2],
+        round: true
+        },
+    duration: 0
+})
 
-    anime({ 
-        targets: '.checkbox .on', 
-        translateX: '0',
-        zIndex: {
-            value: [1, 2],
-            round: true
-          },
-        duration: 0
-    })
-    anime({ 
-        targets: '.checkbox .off', 
-        translateX: '-100%',
-        zIndex: {
-            value: [2, 1],
-            round: true
-          },
-        duration: 0
-    })
+anime({ 
+    targets: checkboxOff, 
+    translateX: '-100%',
+    zIndex: {
+        value: [2, 1],
+        round: true
+        },
+    duration: 0
+})
 
-    var trigger = true;
-    $('body').on('click', '.checkbox label', function () {
-        if (trigger) {
-            $('.checkbox').toggleClass('active');
-            trigger = false;
+checkboxLabel.addEventListener('click', () => {
+    if (trigger) {
+        checkbox.classList.toggle('active');
+        trigger = false;
 
-            if ($('.checkbox').hasClass('active')) {
-                $('.checkbox').prop('checked', false);
+        if (checkbox.classList.contains('active')) {
+            checkbox.setAttribute('checked', false);
 
-                anime({ 
-                    targets: '.checkbox .off', 
-                    zIndex: {
-                        value: [1, 2],
-                        round: true
-                      },
-                    duration: 0
-                })
-                anime({ 
-                    targets: '.checkbox .on', 
-                    zIndex: {
-                        value: [2, 1],
-                        round: true
-                      },
-                    duration: 0
-                })
-    
-                anime({ 
-                    targets: '.checkbox .off', 
-                    translateX: '0%',
-                    duration: 500,
-                    easing: 'easeInOutQuad',
-                    complete: function() {
-                        anime({ 
-                            targets: '.checkbox .on', 
-                            translateX: '100%',
-                            duration: 0
-                        });
-                        trigger = true;
-                    }
-                })
-            } else {
-                $('.checkbox').prop('checked', true);
+            anime({
+                targets: checkboxOff,
+                zIndex: {
+                    value: [1, 2],
+                    round: true
+                },
+                duration: 0
+            })
+            anime({
+                targets: checkboxOn,
+                zIndex: {
+                    value: [2, 1],
+                    round: true
+                },
+                duration: 0
+            })
 
-                anime({ 
-                    targets: '.checkbox .on', 
-                    zIndex: {
-                        value: [1, 2],
-                        round: true
-                    },
-                    duration: 0
-                })
-                anime({ 
-                    targets: '.checkbox .off', 
-                    zIndex: {
-                        value: [2, 1],
-                        round: true
-                    },
-                    duration: 0
-                })
-    
-                anime({ 
-                    targets: '.checkbox .on', 
-                    translateX: '0%',
-                    duration: 500,
-                    easing: 'easeInOutQuad',
-                    complete: function() {
-                        anime({ 
-                            targets: '.checkbox .off', 
-                            translateX: '-100%',
-                            duration: 0
-                        });
-                        trigger = true;
-                    }
-                })
-            }
+            anime({
+                targets: checkboxOff,
+                translateX: '0%',
+                duration: 500,
+                easing: 'easeInOutQuad',
+                complete: () => {
+                    anime({
+                        targets: checkboxOn,
+                        translateX: '100%',
+                        duration: 0
+                    });
+                    trigger = true;
+                }
+            })
+        } else {
+            checkbox.setAttribute('checked', true);
+
+            anime({
+                targets: checkboxOn,
+                zIndex: {
+                    value: [1, 2],
+                    round: true
+                },
+                duration: 0
+            })
+            anime({
+                targets: checkboxOff,
+                zIndex: {
+                    value: [2, 1],
+                    round: true
+                },
+                duration: 0
+            })
+
+            anime({
+                targets: checkboxOn,
+                translateX: '0%',
+                duration: 500,
+                easing: 'easeInOutQuad',
+                complete: () => {
+                    anime({
+                        targets: checkboxOff,
+                        translateX: '-100%',
+                        duration: 0
+                    });
+                    trigger = true;
+                }
+            })
         }
-    });
+    }
+});
 
 
-    
+let switcher = document.querySelector('.switcher');
+let switcherLabel = document.querySelector('.switcher label');
+let switcherLeft = document.querySelector('.switcher .left');
+let switcherRight = document.querySelector('.switcher .right');
+let zindex = 2;
+let t = true;
 
-    var zindex = 2;
-    var t = true;
-    $('body').on('click', '.switcher label', function () {
-        if (t) {
-            t = false;
-            $('.switcher').toggleClass('active');
-            zindex++;
+switcherLabel.addEventListener('click', () => {
+    if (t) {
+        t = false;
 
-            if ($('.switcher').hasClass('active')) {
-                anime({
-                    targets: '.switcher .left',
-                    scale: 12,
-                    duration: 500,
-                    easing: 'easeInOutQuad',
-                    complete: function () {
-                        anime({
-                            targets: '.switcher .right',
-                            scale: 0,
-                            zIndex: {
-                                value: [zindex, zindex++],
-                                round: true
-                            },
-                            duration: 0
-                        });
+        switcher.classList.toggle('active');
+        zindex++;
 
-                        anime({
-                            targets: '.switcher .right',
-                            scale: 1,
-                            duration: 700,
-                            complete: function () {
-                                t = true;
-                            }
-                        });                        
-                    }
-                })
-            } else {
-                anime({
-                    targets: '.switcher .right',
-                    scale: 12,
-                    duration: 500,
-                    easing: 'easeInOutQuad',
-                    complete: function () {
-                        anime({
-                            targets: '.switcher .left',
-                            scale: 0,
-                            zIndex: {
-                                value: [zindex, zindex++],
-                                round: true
-                            },
-                            duration: 0
-                        });
+        if (switcher.classList.contains('active')) {
+            anime({
+                targets: switcherLeft,
+                scale: 12,
+                duration: 500,
+                easing: 'easeInOutQuad',
+                complete: () => {
+                    anime({
+                        targets: switcherRight,
+                        scale: 0,
+                        zIndex: {
+                            value: [zindex, zindex++],
+                            round: true
+                        },
+                        duration: 0
+                    });
 
-                        anime({
-                            targets: '.switcher .left',
-                            scale: 1,
-                            duration: 700,
-                            complete: function () {
-                                t = true;
-                            }
-                        });
-                    }
-                })
-            }
+                    anime({
+                        targets: switcherRight,
+                        scale: 1,
+                        duration: 700,
+                        complete: () => {
+                            t = true;
+                        }
+                    });
+                }
+            })
+        } else {
+            anime({
+                targets: switcherRight,
+                scale: 12,
+                duration: 500,
+                easing: 'easeInOutQuad',
+                complete: () => {
+                    anime({
+                        targets: switcherLeft,
+                        scale: 0,
+                        zIndex: {
+                            value: [zindex, zindex++],
+                            round: true
+                        },
+                        duration: 0
+                    });
+
+                    anime({
+                        targets: switcherLeft,
+                        scale: 1,
+                        duration: 700,
+                        complete: () => {
+                            t = true;
+                        }
+                    });
+                }
+            })
         }
-       
-    });
+    }
 });
